1555eb2d4089e089d7565993322b57cd7a7b115f
NicolasAbarletta <nicolasabarletta@myacapital.com>
2025-08-05 16:31:15 -0500

    Delete backend directory

--- backend/src/backend/index.ts
+++
@@ -1,8 +0,0 @@
-import app from './server';
-
-const PORT = process.env.PORT || 10000;
-
-app.listen(PORT, () => {
-  // eslint-disable-next-line no-console
-  console.log(`FinDash backend listening on port ${PORT}`);
-});

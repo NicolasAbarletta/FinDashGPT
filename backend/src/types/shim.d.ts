c32c0e29175a73a3e4f5386e81e0c4b0748b016d
NicolasAbarletta <nicolasabarletta@myacapital.com>
2025-08-05 13:56:45 -0500

    Update shim.d.ts

--- backend/src/types/shim.d.ts
+++ backend/src/types/shim.d.ts
@@ -1,3 +1,10 @@
-declare module 'node-cron';
-declare module 'better-sqlite3';
+// Tell TypeScript to accept any import from these CJS modules
+declare module 'node-cron' {
+  const whatever: any;
+  export = whatever;
+}
 
+declare module 'better-sqlite3' {
+  const Database: any;
+  export = Database;
+}

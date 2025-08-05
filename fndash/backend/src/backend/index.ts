23d331b318e601d57b6b65d55399374ac8766fcd
NicolasAbarletta <nicolasabarletta@myacapital.com>
2025-08-05 13:36:20 -0500

    Delete fndash directory

--- fndash/backend/src/backend/index.ts
+++
@@ -1,8 +0,0 @@
-import server from './server';
-import './cron';
-
-const port = process.env.PORT || 3001;
-server.listen(port, () => {
-  // eslint-disable-next-line no-console
-  console.log(`FinDash backend listening on port ${port}`);
-});

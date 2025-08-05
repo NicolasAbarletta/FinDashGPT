4944245f1a5a5bb7f9bb81d8cfbcf4d556440f91
NicolasAbarletta <nicolasabarletta@myacapital.com>
2025-08-05 17:21:12 -0500

    Create type.d.ts

---
+++ backend/src/backend/type.d.ts
@@ -0,0 +1,9 @@
+// backend/src/backend/types.d.ts
+
+// Declare modules that don’t ship with TypeScript definitions.
+// This prevents “could not find a declaration file” errors and
+// tells the compiler to treat these packages as having an `any` type.
+
+declare module 'node-cron';
+declare module 'uuid';
+declare module 'better-sqlite3';
